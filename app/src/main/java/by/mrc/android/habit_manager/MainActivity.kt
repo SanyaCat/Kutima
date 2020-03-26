7edb2354c76f24b83349b3a938bb651164597bc4
Alex Denisevich <SasDenYT@gmail.com>
2020-03-26 11:01:22 +0300

    add share item, add basic localization

--- app/src/main/java/by/mrc/android/habit_manager/MainActivity.kt
+++ app/src/main/java/by/mrc/android/habit_manager/MainActivity.kt
@@ -1,14 +1,18 @@
 package by.mrc.android.habit_manager
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.navigation.findNavController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.ui.*
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import by.mrc.android.habit_manager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -40,10 +44,27 @@ class MainActivity : AppCompatActivity() {
         binding.navView.setupWithNavController(navController)
     }
 
+    // Creating menu
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // TODO("Click options menu")
+        // TODO("Click share")
         menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    // On clicking menu item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // On item Share click
+            R.id.item_share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.setType("text/plain").putExtra(
+                    Intent.EXTRA_TEXT,
+                    Uri.parse(this.getString(R.string.share_intent_content))
+                )
+                startActivity(shareIntent)
+            }
+        }
         return true
     }
 }
